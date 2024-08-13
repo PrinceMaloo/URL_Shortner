f8c36e99849e150669a6cf163d5206a28bae3d5a
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2024-08-13 23:29:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "******",
+    password: "Pm@12345";
     database: "urls_storage"
 });
 
